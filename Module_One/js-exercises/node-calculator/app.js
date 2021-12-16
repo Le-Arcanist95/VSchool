4a3f4c85f12d707f1d486a566a7184065ae88929
Levi Arcane <zarcanist95@gmail.com>
2021-12-15 23:42:39 -0500

    functions for basic math complete

---
+++ Module_One/js-exercises/node-calculator/app.js
@@ -0,0 +1,21 @@
+const readlineSync = require("readline-sync")
+
+function add(num1, num2) {
+    return num1+num2;
+}
+console.log(add(3,5));
+
+function subtract(num1, num2) {
+    return num1-num2;
+}
+console.log(subtract(8,2));
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+console.log(multiply(6,5));
+
+function divide(num1, num2) {
+    return num1/num2;
+}
+console.log(divide(30,10));
