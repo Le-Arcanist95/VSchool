debe17dfe3e9b258c649b3ee1bb7d654067fafb1
Levi Arcane <zarcanist95@gmail.com>
2021-12-16 01:20:26 -0500

    escape room functioning. creating a reset function next

--- Module_One/js-exercises/escape-room/app.js
+++ Module_One/js-exercises/escape-room/app.js
@@ -1,27 +1,29 @@
 const readlineSync = require("readline-sync");
 
-let choice = "";
-let name = readlineSync.question("What is your name? ");
+let isGameOver = null;
+let hasKey = null;
+let isUnlocked = null;
 
-if (name !== "") {
-    console.log("You signed up for this, " + name + ". It was a once in a lifetime offer. You signed a waiver stating that anything to occur within the room is of your own volition. The time limit is one hour to escape before we flood the room with poison gas. Good Luck!")
-    setTimeout(() => {
-        console.log("You have been given three options, in earlier explanation of how this escape room works. Choose wisely: ");
-        console.log("1. Put hand in hole");
-        console.log("2. Find the key");
-        console.log("3. Open the door.");
-    }, 5000)
-    setTimeout(() => {
-        choice = readlineSync.question("What is your selection ..? ")
-    }, 10000)
-}
+const name = readlineSync.question("What is your name? ");
+    console.log("You signed up for this, " + name + ". It was a once in a lifetime offer. You signed a waiver stating that anything to occur within the room is of your own volition. The time limit is one hour to escape before we flood the room with poison gas. Good Luck!");
+    escapeRoom()
 
-switch(choice) {
-    case "1" || "Put hand in hole" || "put hand in hole":
-        console.log("You've died. Try ")
-        break;
-    case "2" || "Find the key" || "find the key":
-        break;
-    case "3" || "Open the door" || "open the door":
-        break;
+function escapeRoom() {
+    console.log("Choose wisely: ", '\n', "1. Put your hand in the hole in the floor.", '\n', "2. Try to solve the puzzle.", '\n', "3. Open the door.");
+
+    const choice = readlineSync.question("What is your selection ..? (Use number to make choice)");
+
+    switch(choice) {
+        case "1":
+            if (Math.random() >= 0.9) {
+                hasKey = true;
+                console.log("You twisted every knob, clicked every button, and lost a few fingers along the way. In the end though, you were able to find the key to escape. Good job.");
+                escapeRoom();
+            } else {
+                hasKey = false;
+                console.log("Better luck next time.");
+                escapeRoom();
+            }
+            break;
+    }
 }
