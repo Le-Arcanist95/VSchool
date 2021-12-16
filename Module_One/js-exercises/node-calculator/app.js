fee315155c5b042e9b6a1e2106f13ae0909d21ef
Levi Arcane <zarcanist95@gmail.com>
2021-12-16 00:06:25 -0500

    node calculator complete

--- Module_One/js-exercises/node-calculator/app.js
+++ Module_One/js-exercises/node-calculator/app.js
@@ -1,23 +1,38 @@
 const readlineSync = require("readline-sync")
 
 function add(num1, num2) {
-    return Number(num1) + Number(num2)
-}
+    return Number(num1) + Number(num2);
+};
 function sub(num1, num2) {
-    return num1 - num2;
-}
+    return Number(num1) - Number(num2);
+};
 function mul(num1, num2) {
-    return num1 * num2;
-}
+    return Number(num1) * Number(num2);
+};
 function div(num1, num2) {
-    return num1 / num2;
-}
+    return Number(num1) / Number(num2);
+};
 
 let num1 = readlineSync.question("Please enter your first number: ");
 let num2 = readlineSync.question("Please enter your second number: ");
-let operator = readlineSync.question("Please enter the operation to perform (add, sub, mul, div): ")
+let operator = readlineSync.question("Please enter the operation to perform (add, sub, mul, div): ");
+let result = "";
 
 switch(operator) {
     case "add":
-        console.log(add(num1,num2))
-}
+        result = add(num1,num2);
+        console.log("The result is: " + result);
+        break;
+    case "sub":
+        result = sub(num1,num2);
+        console.log("The result is: " + result);
+        break;
+    case "mul":
+        result = mul(num1,num2);
+        console.log("The result is: " + result);
+        break;
+    case "div":
+        result = div(num1,num2)
+        console.log("The result is: " + result);
+        break;
+};
