6d686f6a83f1bdbce78d9876c33c38fc65b8b5b8
Levi Arcane <zarcanist95@gmail.com>
2021-12-18 13:24:00 -0500

    added editing functionality

--- Module_One/js-exercises/moms-shopping-list-start/index.js
+++ Module_One/js-exercises/moms-shopping-list-start/index.js
@@ -9,19 +9,36 @@ form.addEventListener('submit', (e) => {
 
     const newItem = document.createElement("li");
     const itemTitle = document.createElement("div");
-    const editButton = document.createElement("button");
     const deleteButton = document.createElement("button");
+    const editButton = document.createElement("button");
+    const editInput = document.createElement("input");
 
     itemTitle.textContent = input.value;
-    editButton.textContent = "edit";
+    editButton.textContent = "Edit";
     deleteButton.textContent = "X";
     input.value = "";
 
     newItem.append(itemTitle, editButton, deleteButton);
     list.append(newItem);
-
+    
     deleteButton.addEventListener('click', (e) => {
         e.target.parentNode.remove();
     });
 
+    editButton.addEventListener('click', (e) => {
+        if (e.target.textContent === "Edit") {
+            let prevValue = itemTitle.textContent;
+            editInput.value = prevValue;
+            itemTitle.textContent = "";
+            editButton.textContent = "Save";
+
+            itemTitle.append(editInput);
+        } else if (e.target.textContent === "Save") {
+            itemTitle.textContent = editInput.value;
+            editButton.textContent = "Edit";
+            editInput.remove();
+        };
+    });
 });
+
+
