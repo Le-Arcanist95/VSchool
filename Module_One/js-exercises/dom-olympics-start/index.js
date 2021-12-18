64875053cb4b253e3720f9e6803488411bb69067
Levi Arcane <zarcanist95@gmail.com>
2021-12-17 23:21:01 -0500

    bronze medal complete

--- Module_One/js-exercises/dom-olympics-start/index.js
+++ Module_One/js-exercises/dom-olympics-start/index.js
@@ -3,19 +3,38 @@ const h1 = document.createElement("h1");
 const h3 = document.createElement("h3");
 const nameSpan = document.createElement("span");
 const creditSpan = document.createElement("span");
+const messageContainer = document.getElementById("main");
+const messages = document.querySelector(".messages");
+const message = document.getElementsByClassName("message");
+const lOrient = document.querySelectorAll(".left");
+const rOrient = document.querySelectorAll(".right");
+const themeSelect = document.getElementById("theme-drop-down");
 const sendButton = document.getElementById("send-button");
 const clearButton = document.getElementById("clear-button");
 const funButton = document.createElement("button");
-const message = document.getElementsByClassName("message");
-const messages = document.querySelectorAll(".messages");
-const themeSelect = document.getElementById("theme-drop-down");
-const lOrient = document.querySelectorAll(".left");
-const rOrient = document.querySelectorAll(".right");
 const input = document.getElementById("input");
 
-
-
-
 h1.textContent = "JavaScript Made This!!";
-h4.textContent = "Levi Arcane wrote the JavaScript";
-header.append(h1, h4);
+nameSpan.textContent = "Levi Arcane ";
+creditSpan.textContent = "wrote the Javascript";
+funButton.textContent = "Make it Fun"
+
+h3.append(nameSpan, creditSpan);
+header.append(h1, h3);
+
+funButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    for (let i = 0; i < message.length; i++) {
+        const funArr = ["Raindrops on Roses", "Whiskers on Kittens", "Bright Copper Kettles", "Warm Woolen Mittens", "Cream-Colored Ponies", "Crisp Apple Strudels", "Doorbells and Sleighbells", "Schnitzel with Noodles"];
+
+        message[i].textContent = funArr[i];
+    }
+});
+clearButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    messages.innerHTML = "";
+})
+
+messageContainer.append(funButton);
