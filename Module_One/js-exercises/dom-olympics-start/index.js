e1726605c4148fa48287fb9b16c82024a33edd22
Levi Arcane <zarcanist95@gmail.com>
2021-12-17 20:52:52 -0500

    created variables linking to various parts of the DOM

--- Module_One/js-exercises/dom-olympics-start/index.js
+++ Module_One/js-exercises/dom-olympics-start/index.js
@@ -1,8 +1,21 @@
 const header = document.getElementById("header")
 const h1 = document.createElement("h1");
-const h4 = document.createElement("h4");
-const span = document.createElement("span");
+const h3 = document.createElement("h3");
+const nameSpan = document.createElement("span");
+const creditSpan = document.createElement("span");
+const sendButton = document.getElementById("send-button");
+const clearButton = document.getElementById("clear-button");
+const funButton = document.createElement("button");
+const message = document.getElementsByClassName("message");
+const messages = document.querySelectorAll(".messages");
+const themeSelect = document.getElementById("theme-drop-down");
+const lOrient = document.querySelectorAll(".left");
+const rOrient = document.querySelectorAll(".right");
+const input = document.getElementById("input");
+
+
+
 
 h1.textContent = "JavaScript Made This!!";
 h4.textContent = "Levi Arcane wrote the JavaScript";
-header.append(h1, h4)
+header.append(h1, h4);
