51759362dfe13377a642028746df388b07507942
Levi Arcane <zarcanist95@gmail.com>
2021-12-24 14:15:35 -0500

    squashed all bugs on the plane

--- Module_One/assignments/bugs-on-a-plane/app.js
+++ Module_One/assignments/bugs-on-a-plane/app.js
@@ -1,14 +1,14 @@
-var form = document.getElementByID("arline-form");
-var submit = document.getElementByID(submit);
-var query = document.querySelector;
+const form = document.getElementById("airline-form")
+const submit = document.getElementById("submit");
+const query = document.querySelector;
 
 function formAlert() {
-    var firstName = form.elements["firstName"].value;
-    var lastName = form.elements["lastName"].value;
-    var age = form.elements["age"].value;
-    var gender = form.elements["gender"].value;
-    var location = form.elements["travel-location"].value;
-    var diet = {};
+    let firstName = form.elements["first-name"].value;
+    let lastName = form.elements["last-name"].value;
+    let age = form.elements["age"].value;
+    let gender = form.elements["gender"].value;
+    let location = form.elements["travel-location"].value;
+    let diet = [];
     if (form.elements['vegan'].checked) {
         diet.push(document.getElementById("vegan").value);
     }
@@ -20,7 +20,7 @@ function formAlert() {
     }
 
 
-    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+    return alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
 }
 
 
