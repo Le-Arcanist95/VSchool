90ac77bcfcf8f5de0f2457d6f5532db98080c80f
Levi Arcane <zarcanist95@gmail.com>
2021-12-24 16:26:27 -0500

    Added array object for maintaining counts of fizz and buzz

--- Module_One/js-exercises/fizzbuzz/index.js
+++ Module_One/js-exercises/fizzbuzz/index.js
@@ -1,10 +1,11 @@
-countArrObj = {
+const countArrObj = {
     fizzBuzz: 0,
     fizz: 0,
     buzz: 0
 }
 
-for(let i = 0; i < 100;)
+for(let i = 0; i < 100;) {
     console.log(
         (++i % 3 ? "" : "Fizz" ) + ( i%5 ? "" : "Buzz" ) || i 
     )
+}
