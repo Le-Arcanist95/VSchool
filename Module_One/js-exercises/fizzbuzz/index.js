3fc637e35c1a9031f99f7103718cffd30bf45914
Levi Arcane <zarcanist95@gmail.com>
2021-12-24 18:30:17 -0500

    created conditionals to count variations in i

--- Module_One/js-exercises/fizzbuzz/index.js
+++ Module_One/js-exercises/fizzbuzz/index.js
@@ -1,11 +1,26 @@
 const countArrObj = {
-    fizzBuzz: 0,
+    fizzbuzz: 0,
     fizz: 0,
     buzz: 0
 }
 
 for(let i = 0; i < 100;) {
-    console.log(
-        (++i % 3 ? "" : "Fizz" ) + ( i%5 ? "" : "Buzz" ) || i 
-    )
-}
+    let value = i;
+
+    value = ((++i%3 ? '' : "fizz") + (i%5 ? '' : "buzz"));
+
+    if (value === "fizzbuzz") {
+        countArrObj.fizzbuzz++;
+        console.log(value);
+    } else if (value === "fizz") {
+        countArrObj.fizz++;
+        console.log(value);
+    } else if (value === "buzz") {
+        countArrObj.buzz++;
+        console.log(value);
+    } else {
+        console.log(i)
+    };
+};
+
+console.log(countArrObj);
