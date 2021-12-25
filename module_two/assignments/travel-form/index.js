99310620484a4b90e01fe89c51f50b66294c7284
Levi Arcane <zarcanist95@gmail.com>
2021-12-24 23:18:53 -0500

    Travel Form complete

---
+++ module_two/assignments/travel-form/index.js
@@ -0,0 +1,30 @@
+const submit = document.getElementById("submit");
+
+function createAlert() {
+    const firstName = document.travelForm.firstName.value;
+    const lastName = document.travelForm.lastName.value;
+    const age = document.travelForm.age.value;
+    const gender = document.travelForm.gender.value;
+    const location = document.travelForm.travelLocation.value;
+    const diet = [];
+        if(travelForm.diabetic.checked){
+            diet.push(document.travelForm.diabetic.value);
+        };
+        if(travelForm.gluten.checked){
+            diet.push(document.travelForm.gluten.value);
+        };
+        if(travelForm.peanut.checked){
+            diet.push(document.travelForm.peanut.value);
+        };
+
+        alert(
+            "First Name: " + firstName + 
+            "\nLast Name: " + lastName + 
+            "\nAge: " + age + 
+            "\nGender: " + gender + 
+            "\nTravel Location: " + location + 
+            "\nDiet: " + diet
+        );
+};
+
+submit.addEventListener("click", createAlert);
