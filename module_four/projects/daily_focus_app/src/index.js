521a128aa336c8cce618565dc7d5b66cf88ae232
Levi Arcane <zarcanist95@gmail.com>
2022-09-15 15:18:58 -0400

    Completed journal page with minor visual shortcoming. Updated css to scss and installed sass dependency.

--- module_four/projects/daily_focus_app/src/index.js
+++ module_four/projects/daily_focus_app/src/index.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import Journal from "./pages/Journal";
 import TaskList from "./pages/TaskList";
 import Task from "./pages/Task"
-import "./index.css";
+import "./index.scss";
 
 const root = createRoot(document.getElementById("root"));
 
