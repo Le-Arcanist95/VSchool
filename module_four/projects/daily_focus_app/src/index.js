6ef96309c67a3f69ee3c02250c2e19cc0abbb47e
Levi Arcane <zarcanist95@gmail.com>
2022-09-18 16:47:00 -0400

    Updated React Router to utilite RouterProvider

--- module_four/projects/daily_focus_app/src/index.js
+++ module_four/projects/daily_focus_app/src/index.js
@@ -1,29 +1,36 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { 
+    createBrowserRouter, 
+    createRoutesFromElements,
+    RouterProvider, 
+    Route 
+} from "react-router-dom";
 import App from "./App";
-import Home from "./pages/Home";
-import Journal from "./pages/Journal";
-import TaskList from "./pages/TaskList";
-import Task from "./pages/Task"
+import Home from "./routes/Home";
+import Journal from "./routes/Journal";
+import TaskList from "./routes/TaskList";
+import Task from "./routes/Task"
 import "./index.scss";
 
 const root = createRoot(document.getElementById("root"));
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route index element={<Home />}/>
+            <Route path="journal" element={<Journal />}/>
+            <Route path="tasks" element={<TaskList />}>
+                <Route path=":taskId" element={<Task/>}/>
+            </Route>
+            <Route path="*" element={
+                <main style={{padding: "1em"}}>
+                    <p>Oops! You've reached an empty page!</p>
+                </main>
+            }/>
+        </Route>
+    )
+);
 
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}>
-                <Route index element={<Home />}/>
-                <Route path="journal" element={<Journal />}/>
-                <Route path="tasks" element={<TaskList />}>
-                    <Route path=":taskId" element={<Task/>}/>
-                </Route>
-                <Route path="*" element={
-                    <main style={{padding: "1em"}}>
-                        <p>Oops! You've reached an empty page!</p>
-                    </main>
-                }/>
-            </Route>
-        </Routes>
-    </BrowserRouter>
-);
+    <RouterProvider router={ router } />
+)
