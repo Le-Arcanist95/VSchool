fc5859c2293134ea5703e38ef5b74e2db6a4c009
Levi Arcane <zarcanist95@gmail.com>
2022-11-25 10:51:39 -0500

    Added uuidv4 support for unique ID assignments

--- module_five/notes/SERVER_SIDE_PROGRAMMING/server.js
+++ module_five/notes/SERVER_SIDE_PROGRAMMING/server.js
@@ -1,20 +1,31 @@
 const express = require("express");
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 
-const users = [
-    { name: "harry", age: 63 },
-    { name: "larry", age: 19 },
-    { name: "barry", age: 27 },
-    { name: "cherry", age: 8 },
-    { name: "fairy", age: 999 }
-]
+// Middleware (for every request)
+app.use(express.json()); // Looks for a request body, and turns it into 'req.body'
 
+// Fake Data
+const users = [
+    { name: "harry", age: 63, _id: uuidv4()},
+    { name: "larry", age: 19, _id: uuidv4()},
+    { name: "barry", age: 27, _id: uuidv4()},
+    { name: "cherry", age: 8, _id: uuidv4()},
+    { name: "fairy", age: 999, _id: uuidv4()}
+];
+
+// Routes
 app.get('/users', (req, res) => {
     res.send(users);
 });
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser._id = uuidv4();
+    users.push(newUser);
+    res.send(`Successfully added ${req.body.name} to the database.`);
+});
 
-
-    // 1: PORT  2: CB
+// Server listening
 app.listen(4321, () => {
     console.log("The server is running on Port 4321");
 });
