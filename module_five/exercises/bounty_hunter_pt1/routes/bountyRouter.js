55e2452ad0b830972b04eb3e7b553a8e09a68baa
Levi Arcane <zarcanist95@gmail.com>
2022-11-25 11:59:13 -0500

    Separated bounties into individual route file

---
+++ module_five/exercises/bounty_hunter_pt1/routes/bountyRouter.js
@@ -0,0 +1,48 @@
+const express = reqiure('express');
+const { v4: uuidv4 } = require('uuid');
+const bountyRouter = express.Router();
+
+
+const bounties = [
+    {
+        fName: "Luke",
+        lName: "Skywalker",
+        living: true,
+        bounty: 300,
+        type: "Jedi",
+        _id: uuidv4()
+    },
+    {
+        fName: "Darth",
+        lName: "Vader",
+        living: false,
+        bounty: 500,
+        type: "Sith",
+        _id: uuidv4()
+    }
+];
+
+app.route('/')
+    .get((req, res) => {
+        res.send(bounties);
+    })
+    .post((req, res) => {
+        const newBounty = req.body;
+        newBounty._id = uuidv4();
+        bounties.push(newBounty);
+        console.log(`Successfully added ${newBounty.fName} ${newBounty.lName} to the hitlist.`);
+    });
+app.route('/:bountyId')
+    .get((req, res) => {
+        const reqItem = bounties.find(bounty => bounty._id = req.params.bountyId);
+        res.send(reqItem);
+    })
+    .put((req, res) => {
+        console.log(`Trying to update ${req.params.bountyId}`);
+    })
+    .delete((req, res) => {
+        console.log(`Trying to delete ${req.params.bountyId}`);
+    });
+
+
+module.exports = bountyRouter;
--- module_five/exercises/bounty_hunter_pt1/server.js
+++ module_five/exercises/bounty_hunter_pt1/server.js
@@ -1,45 +1,8 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = process.env.PORT || 1234;
 
-const bounties = [
-    {
-        fName: "Luke",
-        lName: "Skywalker",
-        living: true,
-        bounty: 300,
-        type: "Jedi",
-        _id: uuidv4()
-    },
-    {
-        fName: "Darth",
-        lName: "Vader",
-        living: false,
-        bounty: 500,
-        type: "Sith",
-        _id: uuidv4()
-    }
-];
-
-app.route('/bounty')
-    .get((req, res) => {
-        res.send(bounties);
-    })
-    .post((req, res) => {
-        const newBounty = req.body;
-        newBounty._id = uuidv4();
-        bounties.push(newBounty);
-        console.log(`Successfully added ${newBounty.fName} ${newBounty.lName} to the hitlist.`);
-    });
-app.route('/bounty/:bountyId')
-    .get((req, res) => {
-        const reqItem = bounties.find(bounty => bounty._id = req.params.bountyId);
-        res.send(reqItem);
-    })
-    .put((req, res) => {
-        
-    })
+app.use( '/bounties', require( './routes/bountyRouter.js' ));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
