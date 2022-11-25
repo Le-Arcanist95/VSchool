a7f3faedec402f7870a705cf2e6a59b5dd7ba296
Levi Arcane <zarcanist95@gmail.com>
2022-11-25 10:16:06 -0500

    Update to notes

--- module_five/notes/SERVER_SIDE_PROGRAMMING/server.js
+++ module_five/notes/SERVER_SIDE_PROGRAMMING/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+const users = [
+    { name: "harry", age: 63 },
+    { name: "larry", age: 19 },
+    { name: "barry", age: 27 },
+    { name: "cherry", age: 8 },
+    { name: "fairy", age: 999 }
+]
+
+app.get('/users', (req, res) => {
+    res.send(users);
 });
 
 
-            // 1: PORT  2: CB
+    // 1: PORT  2: CB
 app.listen(4321, () => {
     console.log("The server is running on Port 4321");
 });
