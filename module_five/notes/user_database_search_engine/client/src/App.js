215d2f12ec9b5f82e2a1a2951697a0459ed3b085
Levi Arcane <zarcanist95@gmail.com>
2022-12-13 18:23:18 -0500

    Initial commit

---
+++ module_five/notes/user_database_search_engine/client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header.js";
+
+export default function App() {
+    return (
+        <div id="page-container">
+            <Header />
+            <div id="content-wrapper">
+
+            </div>
+        </div>
+    )
+}
---
+++ module_five/notes/user_database_search_engine/client/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="header-container">
+            <h1 className="header-title"> User Database Search Engine: Fullstack Project 0 </h1>
+            
+        </div>
+    )
+}
