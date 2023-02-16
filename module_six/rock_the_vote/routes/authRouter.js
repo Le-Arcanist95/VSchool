fe43006dfbffc087a166273fc2984aa74186d37a
Levi Arcane <zarcanist95@gmail.com>
2023-02-16 01:17:40 -0500

    Removed Rock the Vote and made it a separate mod

--- module_six/rock_the_vote/routes/authRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authRouter = express.Router();
-const { register, login } = require('../controllers/AuthController.js');
-
-authRouter.post('/register', register);
-authRouter.post('/login', login);
-
-module.exports = authRouter;
